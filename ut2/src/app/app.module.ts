79177d45e80534d7f460ab3532aba84b3423d56d
mmh <matt.hensinger@gmail.com>
2017-09-15 23:36:39 -0400

    Added header and login components. Added app routing.

--- ut2/src/app/app.module.ts
+++ ut2/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
